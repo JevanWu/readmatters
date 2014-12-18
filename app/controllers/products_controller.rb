8eaa261993e8a5b46709cbfd5bb5f4b15804c3c5
jevanwu <jevanwu@gmail.com>
2014-12-18 22:54:35 +0800

    create product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215143752) do
+ActiveRecord::Schema.define(version: 20141218141253) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.string   "kind"
+    t.decimal  "price"
+    t.text     "description"
+    t.boolean  "sold"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"

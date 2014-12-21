a39bf917b219dbfa32a13cd47ebc014d55652679
jevanwu <jevanwu@gmail.com>
2014-12-21 15:58:48 +0800

    allow inserting name when sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   layout :proper_layout
 
@@ -12,4 +13,10 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :name) }
+    end
 end

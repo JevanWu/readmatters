8d7ad89d10fd2523a97ca3af687a6a5fa846770d
jevanwu <jevanwu@gmail.com>
2014-12-21 15:20:36 +0800

    use different layout for 'sign in' and 'sign up' page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :proper_layout
+
+  def proper_layout
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end

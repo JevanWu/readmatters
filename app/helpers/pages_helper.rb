0bf17b86e0c09a2171a2f612e82a3ce996613c65
jevanwu <jevanwu@gmail.com>
2015-01-31 13:42:32 +0800

    set current cart helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+
+  def current_cart
+    Cart.find(session[:card_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:card_id] = cart.id
+    cart
+  end
 end

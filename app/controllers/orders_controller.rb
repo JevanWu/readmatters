3892c08450fd941273bf7723db7c7ef7b38d7345
jevanwu <jevanwu@gmail.com>
2015-02-11 18:35:45 +0800

    order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,38 @@
 class OrdersController < ApplicationController
 
   def new
-    @order = Order.new
+    @order = current_user.orders.build if current_user.present?
   end
 
   def create
+    @order = Order.create(order_params)
+    binding.pry
+    if clean_cart && set_buyer
+      redirect_to checkout_path
+    else
+      redirect_to :back
+    end
+  end
+
+  def checkout
     
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:receiver_name, :province_id, :city_id, :district_id, :street, :user_id)
+    end
+
+    def clean_cart
+      current_cart.line_items.each do |item|
+        item.order = @order
+        item.cart = nil
+        item.save
+      end
+    end
+
+    def set_buyer
+      @order.receiver_id = current_user.id
+      @order.save
+    end
 end

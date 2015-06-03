ee2f1c981edb750396d32bc9ae01c74ca6af7ade
jevanwu <jevanwu@gmail.com>
2015-06-03 22:31:04 +0800

    user settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,13 @@ class UsersController < ApplicationController
       format.js { render "update_partial", layout: false }
     end
   end
+
+  def setting
+    @partial = "setting"
+    @user = current_user
+    respond_to do |format|
+      format.html
+      format.js { render "update_partial", layout: false }
+    end
+  end
 end

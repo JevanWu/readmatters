e4a9b2f175398b86bfedf97f71859c10531cb780
jevanwu <jevanwu@gmail.com>
2015-06-06 23:24:49 +0800

    add the estimation drag slider

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( devise_layout.css )
+Rails.application.config.assets.precompile += %w( jquery.nouislider.all.min.js )
+Rails.application.config.assets.precompile += %w( jquery.nouislider.min.css )
+Rails.application.config.assets.precompile += %w( jquery.nouislider.pips.min.css )

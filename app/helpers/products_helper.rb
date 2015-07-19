e8310409d02087f65fa48242bd20dd63c6c50be0
jevanwu <jevanwu@gmail.com>
2015-07-19 13:52:27 +0800

    the trigger to process orders of bought books

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,15 @@
 module ProductsHelper
+
+  def bought_order_trigger(order)
+    case order.state
+    when %(wait_pay)
+      link_to "去付款", checkout_path(order)
+    when %(wait_ship)
+      "卖者在准备发货"
+    when %(wait_confirm)
+      link_to "确认收货", "#"
+    when %(wait_refund)
+      "退款已经在处理中"
+    end
+  end
 end

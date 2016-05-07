507aae57b6a748174de02e8fc936589543e4fc0f
Jevan Wu <jevanwu@gmail.com>
2016-05-07 23:34:18 +0800

    search field modification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
   end
 
   def search
-    @products = Product.where("name LIKE ?", params[:key_word][:book_name])
+    @products = Product.where("name LIKE ?", "%#{params[:key_word][:book_name]}%")
     render "home"
   end
   

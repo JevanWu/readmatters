05fe6071265c314897908164d2e2198af2a5dd88
jevanwu <jevanwu@gmail.com>
2016-05-31 22:41:19 +0800

    add validation to book price

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :price, presence: true
   has_attached_file :avatar, :styles => {:thumb => "200x200#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 

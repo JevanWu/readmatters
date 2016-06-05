d7cde27d274207363c93ed9516c6deed1579cab1
Jevan Wu <jevanwu@gmail.com>
2016-06-05 18:54:48 +0800

    order state modification

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,24 +5,24 @@ module ProductsHelper
     when %(wait_pay)
       link_to "去付款", checkout_path(order)
     when %(wait_ship)
-      "卖者在准备发货"
+      "卖家正在发货..."
     when %(wait_confirm)
       link_to "确认收货", "#"
     when %(wait_refund)
-      "退款已经在处理中"
+      "退款已在处理中"
     end
   end
 
   def sold_order_triggers(order)
     case order.state
     when %(wait_pay)
-      "买者还未付款"
+      "买家还未付款"
     when %(wait_ship)
-      link_to "已经发货", "#"
+      link_to "我已经发货", "#"
     when %(wait_confirm)
-      "等待买者收货"
+      "等待买家收货"
     when %(wait_refund)
-      "买者已经申请退款"
+      "买家已申请退款"
     end
   end
 end

f7caee3fcd90b4d165f23b392965eb0a00510f4f
Jevan Wu <jevanwu@gmail.com>
2016-06-19 17:37:50 +0800

    merge code piece

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,7 +7,7 @@ module ProductsHelper
     when %(wait_ship)
       "卖家正在发货..."
     when %(wait_confirm)
-      link_to "确认收货", "#"
+      link_to "确认收货", confirm_order_path(order.identifier), remote: true, method: :post
     when %(wait_refund)
       "退款已在处理中"
     end
@@ -18,11 +18,27 @@ module ProductsHelper
     when %(wait_pay)
       "买家还未付款"
     when %(wait_ship)
-      link_to "我已经发货", "#"
+      link_to "我已经发货", ship_order_path(order.identifier), remote: true, method: :post
     when %(wait_confirm)
       "等待买家收货"
     when %(wait_refund)
       "买家已申请退款"
+    end
+  end
+
+  def order_triggers(order)
+    if order.buyer == current_user
+      bought_order_triggers(order)
+    elsif order.seller == current_user
+      sold_order_triggers(order)
+    end
+  end
+
+  def order_inscribe(order)
+    if order.buyer == current_user
+      "From: #{order.seller.name}"
+    elsif order.seller == current_user
+      "To: #{order.receiver_name}"
     end
   end
 end

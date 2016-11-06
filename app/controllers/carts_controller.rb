2821170232eea762adf395ca4354a719bf574a2e
Jevan Wu <jevanwu@gmail.com>
2016-11-06 19:26:27 +0800

    calculate the total price and show up in the page of carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,13 @@
 class CartsController < ApplicationController
 
   def show
-    @cart = current_cart
-    sellers = @cart.products.map(&:user)
+    sellers = @current_cart.products.map(&:user).uniq
     @line_item_grouped_hash = {}
+    @total_prices = []
     sellers.each do |seller|
-      @line_item_grouped_hash["#{seller.id},#{seller.name}"] = @cart.line_items.eager_load(:product).where("products.user_id = ?", seller.id)
+      line_items = @current_cart.line_items.eager_load(:product).where("products.user_id = ?", seller.id)
+      @total_prices << Product.where(id: line_items.pluck(:product_id)).pluck(:price).inject(0, :+)
+      @line_item_grouped_hash["#{seller.id},#{seller.name}"] = line_items
     end
   end
 end

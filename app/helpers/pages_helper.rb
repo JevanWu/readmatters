33df414acc3158fbacea6a2000e3faf74bfcad0d
Jevan Wu <jevanwu@gmail.com>
2016-11-06 18:09:30 +0800

    refactor cart mechanism, make it linked to the account if user signed in

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,6 @@
 module PagesHelper
 
   def current_cart
-    Cart.find(session[:card_id])
-  rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:card_id] = cart.id
-    cart
+    @current_cart
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106044750) do
+ActiveRecord::Schema.define(version: 20161106095104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,10 @@ ActiveRecord::Schema.define(version: 20161106044750) do
   create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id", using: :btree
 
   create_table "cities", force: true do |t|
     t.string   "name"

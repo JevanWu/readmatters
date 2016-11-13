bf90eec6954966b8c39b86d45eb5d2a9cf836f70
Jevan Wu <jevanwu@gmail.com>
2016-11-13 12:19:39 +0800

    style modification

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,11 +3,11 @@ module ProductsHelper
   def bought_order_triggers(order)
     case order.state
     when %(wait_pay)
-      link_to "去付款", checkout_path(order)
+      link_to "去付款", checkout_path(order), class: "theme-link"
     when %(wait_ship)
       "卖家正在发货..."
     when %(wait_confirm)
-      link_to "确认收货", confirm_order_path(order.identifier), remote: true, method: :post
+      link_to "确认收货", confirm_order_path(order.identifier), remote: true, method: :post, class: "theme-link"
     when %(wait_refund)
       "退款已在处理中"
     end
@@ -18,7 +18,7 @@ module ProductsHelper
     when %(wait_pay)
       "买家还未付款"
     when %(wait_ship)
-      link_to "我已经发货", ship_order_path(order.identifier), remote: true, method: :post
+      link_to "我已经发货", ship_order_path(order.identifier), remote: true, method: :post, class: "theme-link"
     when %(wait_confirm)
       "等待买家确认"
     when %(wait_refund)

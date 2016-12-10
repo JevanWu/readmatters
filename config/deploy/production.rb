d6308bc0b0bb43678185580ef759f4edd87d5fbb
Jevan Wu <jevanwu@gmail.com>
2016-12-10 21:28:17 +0800

    change server address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '128.199.75.141', user: 'root', roles: %w{app db web}
+server '188.166.233.84', user: 'root', roles: %w{app db web}
 
 set :rails_env, "production"
 

d2d010293dbc604c85feb1a151c8b930e1f56802
Jevan Wu <jevanwu@gmail.com>
2016-12-11 22:28:58 +0800

    add sendcloud config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :sendcloud
+  config.action_mailer.sendcloud_settings = {
+    :api_user => ENV["SENDCLOUD_API_USER"],
+    :api_key => ENV["SENDCLOUD_API_KEY"]
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

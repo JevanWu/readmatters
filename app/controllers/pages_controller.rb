bd7575fd80325d5402fae0e698eebd6d3b3fbbec
Jevan Wu <jevanwu@gmail.com>
2016-12-22 22:06:27 +0800

    add original price to landing product boxes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @cart = current_cart
-    @products = Product.available
+    @products = Product.available.eager_load(:book)
   end
 
   def search

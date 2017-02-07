8cb354b02cd8eb3d283853aa94b1a1d51d18c40d
Jevan Wu <jevanwu@gmail.com>
2017-02-07 22:32:45 +0800

    chore

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,14 +7,14 @@ class PagesController < ApplicationController
 
   def search
     book_name = params[:key_word][:book_name]
-    @products = Product.eager_load(:book).where("lower(books.name) LIKE ?", "%#{book_name.downcase}%")
+    @products = Product.available.eager_load(:book).where("lower(books.name) LIKE ?", "%#{book_name.downcase}%")
     @title = "关键字：#{book_name}"
     render "home"
   end
 
   def owner
     user = User.find(params[:id])
-    @products = user.products.eager_load(:book)
+    @products = user.products.available.eager_load(:book)
     @title = "#{user.name}的书籍"
     render "home"
   end

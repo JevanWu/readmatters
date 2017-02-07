1164cfbee5bf8bd922140787268fa21b8611fa97
Jevan Wu <jevanwu@gmail.com>
2017-02-07 18:43:11 +0800

    make the title of home page changeable depending on specific page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,16 @@ class PagesController < ApplicationController
   end
 
   def search
-    @products = Product.eager_load(:book).where("lower(books.name) LIKE ?", "%#{params[:key_word][:book_name].downcase}%")
+    book_name = params[:key_word][:book_name]
+    @products = Product.eager_load(:book).where("lower(books.name) LIKE ?", "%#{book_name.downcase}%")
+    @title = "关键字：#{book_name}"
     render "home"
   end
 
   def owner
     user = User.find(params[:id])
     @products = user.products.eager_load(:book)
+    @title = "#{user.name}的书籍"
     render "home"
   end
 end

f634c439fe8e0969563cd3c6c8f52446b9760b19
Jevan Wu <jevanwu@gmail.com>
2017-03-01 13:39:47 +0800

    using qiniu as the paperclip storage only on production environment

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,15 +1,17 @@
-Paperclip::Attachment.default_options[:storage] = :qiniu
-Paperclip::Attachment.default_options[:qiniu_credentials] = {
-  :access_key => ENV["QINIU_ACCESS_TOKEN"],
-  :secret_key => ENV["QINIU_SECRET_TOKEN"]
-}
-Paperclip::Attachment.default_options[:bucket] = ENV["QINIU_BUCKET"]
-Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:qiniu_host] = ENV["QINIU_BUCKET_URL"]
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:storage] = :qiniu
+  Paperclip::Attachment.default_options[:qiniu_credentials] = {
+    :access_key => ENV["QINIU_ACCESS_TOKEN"],
+    :secret_key => ENV["QINIU_SECRET_TOKEN"]
+  }
+  Paperclip::Attachment.default_options[:bucket] = ENV["QINIU_BUCKET"]
+  Paperclip::Attachment.default_options[:use_timestamp] = false
+  Paperclip::Attachment.default_options[:qiniu_host] = ENV["QINIU_BUCKET_URL"]
 
-Paperclip.interpolates :image_timestamp do |style, attachment|
+  Paperclip.interpolates :image_timestamp do |style, attachment|
 
-  DateTime.now.strftime("%Y%m%d%H%M%S")
-  # Time.current.to_i
+    DateTime.now.strftime("%Y%m%d%H%M%S")
+    # Time.current.to_i
+  end
 end
 

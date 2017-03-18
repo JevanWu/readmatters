8f40b5c5758b306979e12fca9fea2b757b628dfe
Jevan Wu <jevanwu@gmail.com>
2017-03-18 18:50:08 +0800

    limit the size of uploaded photos of books

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,11 @@ class Photo < ActiveRecord::Base
 
   belongs_to :product
 
-  has_attached_file :image, :default_url => "/images/:style/missing.png", :path => ":class/:attachment/:id_partition/:style/:filename"
+  has_attached_file :image, :default_url => "/images/:style/missing.png",
+                            :styles => { medium: ["100x100>", :jpg]},
+                            :convert_options => { medium: "-quality 75 -strip" },
+                            :path => Figaro.env.paperclip_storage_path,
+                            :url => Figaro.env.paperclip_storage_path
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

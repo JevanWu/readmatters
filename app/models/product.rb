0484b07d002fa5ac683a8ca2eef68a56d5b3afba
Jevan Wu <jevanwu@gmail.com>
2017-04-01 16:37:18 +0800

    if the user is the owner of the product, show up a 'edit' url

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,8 @@ class Product < ActiveRecord::Base
   has_many :line_items
   has_many :photos
 
+  alias :owner :user
+
   validates :price, :tags, :summary, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 

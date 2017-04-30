c6bfaf7900d64056de51180a317f30d992c0d8b9
Jevan Wu <jevanwu@gmail.com>
2017-04-30 21:05:03 +0800

    code modification of social share helper

--- app/helpers/social_share_helper.rb
+++ app/helpers/social_share_helper.rb
@@ -1,16 +1,24 @@
 module SocialShareHelper
-  def social_share_title(book_name)
-    "我正在Readmatters转售书籍《#{book_name}》"
-  end
-
-  def social_share_summary
+  def social_share_title(product)
+    book_name = product.name
+    if current_user == product.owner
+      "我正在readmatters.com转售书籍《#{book_name}》"
+    else
+      "我正在readmatters.com购买书籍《#{book_name}》"
+    end
   end
 
   def social_share_source
     "http://readmatters.com"
   end
 
-  def social_share_summary(book_name, book_summary)
-    "我正在Readmatters转售书籍《#{book_name}》，快来看看吧。#{book_summary}"
+  def social_share_summary(product)
+    book_name = product.name
+    book_summary = product.book_summary
+    if current_user == product.owner
+      "我正在readmatters.com转售书籍《#{book_name}》，快来看看吧。#{book_summary}"
+    else
+      "我正在readmatters.com购买书籍《#{book_name}》，快来看看吧。#{book_summary}"
+    end
   end
 end

0bcbebe05a7cc1e72f4831fd23b143d834b58191
Jevan Wu <jevanwu@gmail.com>
2017-05-12 23:47:38 +0800

    fix bug

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -13,7 +13,7 @@ class ConversationsController < ApplicationController
     friend_id = params[:friend_id]
     return if friend_id.blank?
     conversation = Conversation.between(current_user.id, friend_id).take
-    conversation.mark_read
+    current_user.mark_read(conversation)
     conversation_html = render_to_string(partial: 'conversations/conversation', locals: { conversation: conversation, user: current_user })
     respond_to do |format|
       format.json { render json: { conversation_html: conversation_html }, status: :ok }

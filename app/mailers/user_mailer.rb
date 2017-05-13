9d41fe6d3ae105a170e7f6d48de2eb243ae47a38
Jevan Wu <jevanwu@gmail.com>
2017-05-13 21:06:08 +0800

    modification of mailer layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+  layout 'mailer'
 
   LABEL_HASH = {
     confirmation_instructions: 139254,

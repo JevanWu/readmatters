02ea82e3848cb34933672bcb4655690e6f1b8a28
Jevan Wu <jevanwu@gmail.com>
2017-06-08 23:15:04 +0800

    modification of image processing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,10 +3,11 @@ class Photo < ApplicationRecord
   belongs_to :product
 
   has_attached_file :image, :default_url => "/images/:style/missing.png",
-                            :styles => { medium: ["100x100>", :jpg]},
-                            :convert_options => { medium: "-quality 75 -strip" },
+                            :styles => { original: ["700x700>", :jpg]},
+                            :convert_options => { original: "-strip" },
                             :path => Figaro.env.paperclip_storage_path,
                             :url => Figaro.env.paperclip_storage_url
+                            # :convert_options => { medium: "-quality 75 -strip" },
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
